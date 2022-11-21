c7ac9b996fcc42bffc72acbd2c3cf539feefd107
Caleb O'Kane <caleb.ceo.okane@gmail.com>
2022-11-21 09:48:31 -0600

    created the file with the parameters for the table

--- src/main/java/com/codeup/springblog/models/Ad.java
+++ src/main/java/com/codeup/springblog/models/Ad.java
@@ -6,4 +6,46 @@ public class Ad {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 250)
+    private String body;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+
+    public Ad() {
+    }
+
+    public Ad(String name, String body) {
+        this.name = name;
+        this.body = body;
+    }
+
+
 }
