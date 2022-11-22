10f1f3ace109b04fda1f8a76f8f281b981583600
Caleb O'Kane <caleb.ceo.okane@gmail.com>
2022-11-22 13:50:23 -0600

    added supplier model

---
+++ src/main/java/com/codeup/springblog/models/Supplier.java
@@ -0,0 +1,42 @@
+package com.codeup.springblog.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "suppliers")
+public class Supplier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Supplier(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Supplier() {
+    }
+
+    public Supplier(String name) {
+        this.name = name;
+    }
+}
